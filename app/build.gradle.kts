9d5f77d6a7c043654e8680715c7f8b2af812e8d5
EA - João Gabriel Santos Mizael <joao_mizael@educadventista.org>
2025-06-09 20:57:17 -0300

    testar usando android studio iguana

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.unasp.financas"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.unasp.financas"
--- app/src/main/java/com/unasp/financas/MainActivity.kt
+++ app/src/main/java/com/unasp/financas/MainActivity.kt
@@ -5,16 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.unasp.financas.model.Financa
+import com.unasp.financas.viewmodel.FinancaViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: FinancaViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        viewModel = ViewModelProvider(this)[FinancaViewModel::class.java]
+
+        val novaFinanca = Financa(
+            nome = "Academia",
+            valor = 59.90,
+            data = "09/06/2025",
+            recorrente = true
+        )
+
+        viewModel.inserir(novaFinanca)
     }
 }
